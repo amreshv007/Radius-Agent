c7e1e8c282ad17f72a40a361fb497104c4118924
amreshv007 <vamresh6@gmail.com>
2019-03-28 13:55:52 +0530

    calculated issues oppenned within a day, within a week and after a week

--- function.php
+++ function.php
@@ -35,7 +35,7 @@ function find_open_issues($url){
 	$nodes = $dom->getElementsByTagName('a');
 	$i = 1;
 	foreach ($nodes as $node) {
-		echo $i." ".$node->nodeValue."<br>";
+		// echo $i." ".$node->nodeValue."<br>";
 		if($i == 53){
 			$open_issues = $node->nodeValue;
 		}
@@ -47,21 +47,85 @@ function find_open_issues($url){
 	if($open_issues == "create an issue"){
 		$open_issues = 0;
 	}
+	else{
+		$r = "";
+		$i = 0;
+		while($i<strlen($open_issues)){
+			if($open_issues[$i]!=','){
+				$r = $r.$open_issues[$i];
+			}
+			$i++;
+		}
+		$open_issues = (int)$r;
+	}
 	echo $open_issues."<br>";
 	return $open_issues;
 }
 
-function find_issues_openned_within_24hrs($url){
-	$page_url = $url;
-	$html = file_get_html($page_url);
+function find_elapsed_days($elapsed){
+	$after = explode(" days", $elapsed);
+	$d = $after[0];
+	$days = (int)$d;
+	return $days;
+}
 
-	foreach($html->find('relative-time') as $element) 
-       echo $element->datetime. '<br>';
-	
+function find_elapsed_days_page_wise($page_url){
+	$html = file_get_html($page_url);
+	$curr_date = date("Y-m-d H:i:s");
+	$tz = new DateTimeZone('Asia/Kolkata');
+	$count1 = 0;
+	$count2 = 0;
+	foreach($html->find('relative-time') as $element){
+		$utc = $element->datetime;
+		$dt = new DateTime($utc);
+       	$dt->setTimezone($tz);
+		// echo $dt->format('Y-m-d H:i:s')."<br>".$curr_date."<br>";
+		$date = $dt->format('Y-m-d H:i:s');
+		$datetime1 = new DateTime();
+		$datetime2 = new DateTime($date);
+		$interval = $datetime1->diff($datetime2);
+		$elapsed = $interval->format('%a days');
+		$days = find_elapsed_days($elapsed);
+		
+		if($days<=0){
+			// echo "Issues openned within 24 Hours!<br>";
+			$count1 = $count1 + 1;
+		}
+		if($days<7 && $days>0){
+			// echo "Issues openned within 1 week!<br>";
+			$count2 = $count2 + 1;
+		}
+		// echo $elapsed."<br><br>";
+	}
+	return $count1." ".$count2;
+}
+
+function find_issues_openned_periodically($url){
+	$count1 = 0;
+	$count2 = 0;
+	$count3 = 0;
+	$i = 0;
+	$page_url = "";
+	while($i<9){
+		if($i == 0){
+			$count = find_elapsed_days_page_wise($url);
+		}
+		else{
+			$j = $i+1;
+			$page_url = $url."?page=$j";
+			echo $page_url."<br>";
+			$count = find_elapsed_days_page_wise($page_url);	
+		}
+		$r = explode(" ", $count);
+		$count1 = $count1 + $r[0];
+		$count2 = $count2 + $r[1];
+		$i++;
+	}
 	
 	echo $url."<br>";
-	
-	$issues = "";
+	echo "count1 = ".$count1." count2=".$count2."<br>";
+
+	$issues = $count1." ".$count2;
 	
 	return $issues;
 }
--- scraped_data.php
+++ scraped_data.php
@@ -1,13 +1,24 @@
 <?php
+date_default_timezone_set('Asia/Kolkata');
+echo date_default_timezone_get()."<br>";
 include_once 'simple_html_dom.php';
 include 'function.php';
-
 $url = $_GET['url'];
-
 $url = filter_url($url);
-
-// $open_issues = find_open_issues($url);
-
-$open_issues_within_24hr = find_issues_openned_within_24hrs($url);
-
+$open_issues = find_open_issues($url);
+$open_issues_periodically = find_issues_openned_periodically($url);
+$r = explode(" ", $open_issues_periodically);
+$issues_within_a_day = (int)$r[0];
+$issues_within_a_week = (int)$r[1];
+$issues_after_a_week = $open_issues - $issues_within_a_week - $issues_within_a_day;
+echo "count1 = ".$issues_within_a_day." count2=".$issues_within_a_week." count3=".$issues_after_a_week."<br>";
 ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php require_once('header.php'); ?>
+	</head>
+	<body id="bodyy">
+		
+	</body>
+</html>
