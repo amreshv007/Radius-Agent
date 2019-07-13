ab365b4612771588baf6d3569ac9118af908e888
Amresh Kumar Verma <vamresh6@gmail.com>
2019-07-13 20:00:23 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,2 +1,14 @@
+		<div class="foot">
+			<div class="col-sm-2">
+				<img alt="" class="mypic" src="https://avatars0.githubusercontent.com/u/25303014?s=460&amp;v=4">
+			</div>
+			<div class="col-sm-10">
+				<p class="f">Amresh Verma</p>
+				<p class="f">Computer Science and Engineering</p>
+				<p class="f">IIIT Jabalpur</p>
+				<p class="f">+91-8318852484</p>
+			</div>
+			
+		</div>
 	</body>
 </html>
