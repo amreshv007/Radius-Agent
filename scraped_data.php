b06a80c3e77bb3e03f746740d775b691f53a0a37
Amresh Kumar Verma <vamresh6@gmail.com>
2019-08-30 17:41:31 +0530

    Update scraped_data.php

--- scraped_data.php
+++ scraped_data.php
@@ -29,8 +29,17 @@ for( $i = 0; $i < sizeof($arr)-1; $i++ ){
 	// each row extraction
 	$operator_country = get_string_between($temp,'<!--','-->');
 	$a = explode(" ", $operator_country);
-	$operator = $a[0];
-	$country = $a[1];
+	$last = sizeof($a)-1;
+	$country = $a[$last];
+	$operator = "";
+	for($i=0;$i<$last-1;$i++){
+		if($i <$last-2){
+			$operator = $operator.$a[$i]." ";		
+		}
+		else{
+			$operator = $operator.$a[$i];	
+		}
+	}
 	$mcc = get_string_between($temp,'mcc="','" mnc');
 	$mnc = get_string_between($temp,'mnc="','"');
 	$volte = get_string_between($temp,'support_volte="','" support_vilte');
