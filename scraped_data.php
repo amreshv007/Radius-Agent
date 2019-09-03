d18954c00309643bd10130db16a3a41e153e7b91
Amresh Kumar Verma <vamresh6@gmail.com>
2019-09-03 13:53:24 +0530

    Update scraped_data.php

--- scraped_data.php
+++ scraped_data.php
@@ -78,13 +78,6 @@ for( $i = 0; $i < sizeof($arr)-1; $i++ ){
 		<?php } ?>
 	</tbody>
 </table>
-<?php
-$file="demo.xls";
-$test="<table  ><tr><td>Cell 1</td><td>Cell 2</td></tr></table>";
-header("Content-type: application/vnd.ms-excel");
-header("Content-Disposition: attachment; filename=$file");
-echo $test;
-?>
 
 <!-- xml task (END) -->
 
