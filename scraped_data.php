202a841ee2961dc5c5be1f2e12e3625ef58d2c7b
Amresh Kumar Verma <vamresh6@gmail.com>
2019-09-03 10:30:55 +0530

    Update scraped_data.php

--- scraped_data.php
+++ scraped_data.php
@@ -6,7 +6,83 @@ include 'function.php';
 
 // xml task (START)
 
-// <!-- xml task (END) -->
+$html = file_get_contents('vo_config_new-d89308866ba8a8ca4df94f4e6344d9d2.xml');
+// print_r($html);
+function get_string_between($string, $start, $end){
+    $string = ' ' . $string;
+    $ini = strpos($string, $start);
+    if ($ini == 0) return '';
+    $ini += strlen($start);
+    $len = strpos($string, $end, $ini) - $ini;
+    return substr($string, $ini, $len);
+}
+// $get_element = get_string_between($html,'<!--','-->');
+$arr = explode('</info>', $html);
+// print_r($arr);
+$first = explode('<info', $arr[0]);
+$arr[0] = "<info".$first[1];
+// echo "0. ".$first[1];
+$table = array();
+for( $i = 0; $i < sizeof($arr)-1; $i++ ){
+// 	echo $i.". ".$arr[$i];
+	$temp = $arr[$i];
+	// each row extraction
+	$operator_country = get_string_between($temp,'<!--','-->');
+	$a = explode(" ", $operator_country);
+	$last = sizeof($a)-1;
+	$country = $a[$last];
+	$operator = "";
+	for($i=0;$i<$last-1;$i++){
+		if($i <$last-2){
+			$operator = $operator.$a[$i]." ";		
+		}
+		else{
+			$operator = $operator.$a[$i];	
+		}
+	}
+	$mcc = get_string_between($temp,'mcc="','" mnc');
+	$mnc = get_string_between($temp,'mnc="','"');
+	$volte = get_string_between($temp,'support_volte="','" support_vilte');
+	$vilte = get_string_between($temp,'support_vilte="','" support_vowifi');
+	$vowifi = get_string_between($temp,'support_vowifi="','" support_viwifi');
+	$viwifi = get_string_between($temp,'support_viwifi="','" />');
+	$table[$i][0] = $country;
+	$table[$i][1] = $operator;
+	$table[$i][2] = $mcc;
+	$table[$i][3] = $mnc;
+	$table[$i][4] = $volte;
+	$table[$i][5] = $vilte;
+	$table[$i][6] = $vowifi;
+	$table[$i][7] = $viwifi;
+}
+?>
+<table class="table">
+	<thead>
+		<tr class="table_head">
+			<th>S. No.</th>
+			<th>Country</th>
+			<th>Operator</th>
+			<th>mcc</th>
+			<th>mnc</th>
+			<th>Volte</th>
+			<th>Vilte</th>
+			<th>Vowifi</th>
+			<th>Viwifi</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php for( $i=0; $i < sizeof($arr)-1; $i++){ ?>
+		<tr class="info">
+			<td><?php echo $i+1; ?></td>
+			<?php for( $j=0; $j<8; $j++) { ?>
+			<td><?php echo $table[$i][$j]; ?></td>
+			<?php } ?>
+		</tr>      
+		<?php } ?>
+	</tbody>
+</table>
+
+<!-- xml task (END) -->
 
 // <?php
 $url = $_GET['url'];
