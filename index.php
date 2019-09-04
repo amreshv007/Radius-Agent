55bcdb0b9256d7a20526fe5fb14e80700b096348
Amresh Kumar Verma <vamresh6@gmail.com>
2019-09-04 16:32:58 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,25 @@
 <?php include 'header.php'; ?>
 <!--  xml task (START) -->
 
-<form action="" method="POST" enctype="multipart/form-data">
+<!-- <form action="" method="POST" enctype="multipart/form-data">
          <input type="file" name="lg_xml" />
          <input type="submit"/>
-</form>
+</form> -->
 
- <?php if(isset($_FILES['lg_xml'])){ ?>
+ <?php //if(isset($_FILES['lg_xml'])){ ?>
 	<h1>Ice Bucket Challenge</h1>
 	<?php include 'simple_html_dom.php'; ?>
 	<?php
 // 	$store = $_FILES['lg_xml']['tmp_name'];
-	$oldPath = $_FILES['lg_xml']['tmp_name'];
+// 	$oldPath = $_FILES['lg_xml']['tmp_name'];
 //      	$newPath = '/tmp/' . basename($_FILES['lg_xml']['name']);
 // 	move_uploaded_file($oldPath, $newPath);
 // 	$get_file = simplexml_load_file($_FILES['lg_xml']['tmp_name']);
 // 	print_r($_FILES['lg_xml']);
 	$html = file_get_contents('vo_config_new-d89308866ba8a8ca4df94f4e6344d9d2.xml');
-	$html1 = file_get_contents($oldPath);
+// 	$html1 = file_get_contents($oldPath);
 // 	print_r($newPath);
-	print_r($html1);
+// 	print_r($html1);
 	function get_string_between($string, $start, $end){
 	    $string = ' ' . $string;
 	    $ini = strpos($string, $start);
@@ -128,7 +128,7 @@
 			<?php } ?>
 	<!-- 	</tbody> -->
 	</table>
-<?php } ?>
+<?php //} ?>
 
 <!-- xml task (END) -->
 
