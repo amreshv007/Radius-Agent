4d933dda5c49e476532e21021f00e06948e9fcaf
Amresh Kumar Verma <vamresh6@gmail.com>
2019-09-05 11:30:03 +0530

    Update form-input.php

--- form-input.php
+++ form-input.php
@@ -1,1 +1,12 @@
-scscscfs
+<?php include 'header.php'; ?>
+<!-- Form input of GitHub public Repository URL -->
+<form name="form" action="scraped_data.php" method="GET" class="formclass">
+	<label class="form_heading">Enter GitHub Repository URL:</label>
+	<label>
+		<input type="text" name="url" class="git_url" placeholder="Enter Name" />
+	</label>
+	<label>
+		<input type="submit" name="submit" value="Submit" />
+	</label>
+</form>
+<?php include 'footer.php'; ?>
