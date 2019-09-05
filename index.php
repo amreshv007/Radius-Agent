0e1f37ed973d091e26cf1f95f55d38692031dbbc
Amresh Kumar Verma <vamresh6@gmail.com>
2019-09-05 09:48:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -12,10 +12,10 @@
 	$oldPath = $_FILES['lg_xml']['tmp_name'];
 	$newPath = ''.basename($_FILES['lg_xml']['name']);
 	if(move_uploaded_file($oldPath, $newPath)){
-		echo "<span style="text-align:center;">Successful Upload!</span>";
+		?> <span style="text-align:center;">Successful Upload!</span> <?php
 	}
 	else{
-		echo "<span style="text-align:center;">Upload Failed!</span>";
+		?> <span style="text-align:center;">Upload Failed!</span> <?php
 	}
 // 	$html = file_get_contents('vo_config_new-d89308866ba8a8ca4df94f4e6344d9d2.xml');
 	$html = file_get_contents($newPath);
