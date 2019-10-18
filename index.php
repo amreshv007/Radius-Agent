1162feca70459f64f871831becb4b9733ca5a746
Amresh Kumar Verma <vamresh6@gmail.com>
2019-10-18 12:26:22 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 include 'simple_html_dom.php';
-$path = 'http://svms.lge.com/pbf/system/navigation/intro.dev';
+$path = 'https://www.google.com/';
 // $p = 
 $html = file_get_contents($path);
 print_r($html);
