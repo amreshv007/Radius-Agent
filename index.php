ce19275e1829eadcd5b3815141af6d0250bacbf3
Amresh Kumar Verma <vamresh6@gmail.com>
2019-10-18 12:30:57 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,18 @@ include 'simple_html_dom.php';
 $path = 'https://www.google.com/';
 // $p = 
 $html = file_get_contents($path);
-print_r($html);
+// print_r($html);
+function get_string_between($string, $start, $end) {
+	    $string = ' ' . $string;
+	    $ini = strpos($string, $start);
+	    if ($ini == 0) return '';
+	    $ini += strlen($start);
+	    $len = strpos($string, $end, $ini) - $ini;
+	    return substr($string, $ini, $len);
+	}
+$arr = explode('<table>', $html);
+print_r($arr);
 echo "hello";
-// print()
+
 include 'footer.php';
 ?>
