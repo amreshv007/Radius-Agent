c3ffd979dadfb3d5ad00ed966c25f60f1eb761e9
Amresh Kumar Verma <vamresh6@gmail.com>
2019-10-18 14:11:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 include 'header.php';
 include 'simple_html_dom.php';
-$path = 'https://www.google.com/';
+$path = 'http://si-rd10-hr09:8080/HRIS/empinfo/myinfo.do?cat=attendanceinfo&action=edit&e=2&fromdate=01%20Oct%202019&todate=10%20Oct%202019/';
 // $p = 
 $html = file_get_contents($path);
 // print_r($html);
