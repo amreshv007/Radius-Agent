12641994e0c039b0cec2e2cdff48428553976849
Amresh Kumar Verma <vamresh6@gmail.com>
2019-10-18 12:22:48 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-// include 'simple_html_dom.php';
-// $path = 'http://si-rd10-hr09:8080/HRIS/empinfo/myinfo.do?cat=attendanceinfo&action=edit&e=2&fromdate=01%20Oct%202019&todate=10%20Oct%202019';
-// // $p = 
-// $html = file_get_contents($path);
-// echo $html;
+include 'simple_html_dom.php';
+$path = 'http://svms.lge.com/pbf/system/navigation/intro.dev';
+// $p = 
+$html = file_get_contents($path);
+echo $html;
 echo "hello";
 // print()
 ?>
