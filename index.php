e94ca31d152fd4af7ce822b464d9a7d465fe0ad0
Amresh Kumar Verma <vamresh6@gmail.com>
2019-10-18 12:19:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 include 'simple_html_dom.php';
-$path = "http://si-rd10-hr09:8080/HRIS/empinfo/myinfo.do?cat=attendanceinfo&action=edit&e=2&fromdate=01%20Oct%202019&todate=10%20Oct%202019";
+$path = 'http://si-rd10-hr09:8080/HRIS/empinfo/myinfo.do?cat=attendanceinfo&action=edit&e=2&fromdate=01%20Oct%202019&todate=10%20Oct%202019';
+// $p = 
 $html = file_get_contents($path);
 print_r($html);
 ?>
